3ee266e87556feaa6b631679bc80d605a3f2a799
José Carlos <jcolscript@gmail.com>
2020-09-22 01:40:10 -0300

    adding context api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
+import AuthContext from './context/AuthContext';
+
 import GlobalStyle from './styles/global';
 
 import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+// import SignUp from './pages/SignUp';
 
 const App: React.FC = () => {
   return (
     <>
-      <SignIn />
+      <AuthContext.Provider value={{ name: 'José Carlos' }}>
+        <SignIn />
+      </AuthContext.Provider>
       <GlobalStyle />
     </>
   );
