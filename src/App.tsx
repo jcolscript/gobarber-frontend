69a3ec32e9c73657e839104d79ad565958104cdc
José Carlos <jcolscript@gmail.com>
2020-09-28 01:18:08 -0300

    creating toasts messages

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { AuthProvider } from './context/AuthContext';
 import GlobalStyle from './styles/global';
 
 import SignIn from './pages/SignIn';
+import ToastContainer from './components/ToastContainer';
 // import SignUp from './pages/SignUp';
 
 const App: React.FC = () => {
@@ -13,6 +14,7 @@ const App: React.FC = () => {
       <AuthProvider>
         <SignIn />
       </AuthProvider>
+      <ToastContainer />
       <GlobalStyle />
     </>
   );
