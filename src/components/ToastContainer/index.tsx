5af244d0331138b3e02249eb0d850f0f6df2f2de
José Carlos <jcolscript@gmail.com>
2020-10-08 00:10:44 -0300

    creating toast component

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,40 +1,26 @@
 import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
 
-import { Container, Toast } from './styles';
+import { Container } from './styles';
 
-const ToastContainer: React.FC = () => {
+import Toast from './Toast';
+
+interface ToastMessage {
+  id: string;
+  type?: 'sucess' | 'error' | 'info';
+  title: string;
+  description?: string;
+}
+
+interface ToastMessageContainer {
+  messages: ToastMessage[];
+}
+
+const ToastContainer: React.FC<ToastMessageContainer> = ({ messages }) => {
   return (
     <Container>
-      <Toast hasDescripiton>
-        <FiAlertCircle size={20} />
-        <div>
-          <strong>Aconteceu um erro!</strong>
-          <p>Não foi possivel fazer login na aplicação</p>
-        </div>
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
-      <Toast type="sucess" hasDescripiton={false}>
-        <FiAlertCircle size={20} />
-        <div>
-          <strong>Aconteceu um erro!</strong>
-        </div>
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
-      <Toast type="error" hasDescripiton>
-        <FiAlertCircle size={20} />
-        <div>
-          <strong>Aconteceu um erro!</strong>
-          <p>Não foi possivel fazer login na aplicação</p>
-        </div>
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
+      {messages.map(message => (
+        <Toast key={message.id} message={message} />
+      ))}
     </Container>
   );
 };
