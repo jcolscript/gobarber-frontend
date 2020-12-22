1f01d365e14734727c5d5e582fa97de3f91b4ac4
José Carlos <jcolscript@gmail.com>
2020-12-21 23:19:41 -0300

    creating reset passaword page

---
+++ src/pages/ResetPassword/index.tsx
@@ -0,0 +1,97 @@
+import React, { useCallback, useRef } from 'react';
+import { FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { useHistory } from 'react-router-dom';
+
+import { useToast } from '../../context/ToastContext';
+import { getValidationErros } from '../../utils/form';
+
+import logo from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, AnimationContainer, Background } from './styles';
+
+interface ResetPassowrdFormData {
+  password: string;
+  confirmPassword: string;
+}
+
+const ResetPassword: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const handleSubmit = useCallback(
+    async (data: ResetPassowrdFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          password: Yup.string().required('Senha obrigatória'),
+          confirmPassword: Yup.string()
+            .oneOf([Yup.ref('password'), undefined], 'Senhas não são iguais')
+            .required('Confirmação obrigatória'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        //Implementar chamada de API
+
+
+        history.push('/signin')
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErros(error);
+          formRef.current?.setErrors(errors);
+          return;
+        }
+        addToast({
+          type: 'error',
+          title: 'Erro ao resetar senha',
+          description: 'Ocorreu um erro ao resetar sua senha, tente novamente.',
+        });
+      }
+    },
+    [addToast, history],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <img src={logo} alt="GoBarber" />
+
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Resetar senha</h1>
+
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Nova senha"
+            />
+
+            <Input
+              name="confirmPassword"
+              icon={FiLock}
+              type="password"
+              placeholder="Confirmação da senha"
+            />
+
+            <Button type="submit">Alterar senha</Button>
+          </Form>
+        </AnimationContainer>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default ResetPassword;
---
+++ src/pages/ResetPassword/styles.ts
@@ -0,0 +1,81 @@
+import styled, { keyframes } from 'styled-components';
+import { shade } from 'polished';
+
+import signInBackImg from '../../assets/sign-in-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  place-content: center;
+  width: 100%;
+  max-width: 700px;
+`;
+
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  } to {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  animation: ${appearFromLeft} 1s;
+
+  form {
+    margin: 80px 0;
+    text-align: center;
+    width: 340px;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+
+  > a {
+    display: flex;
+    align-items: center;
+    color: #ff9000;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    svg {
+      margin-right: 16px;
+    }
+
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signInBackImg}) no-repeat center;
+  background-size: cover;
+`;
